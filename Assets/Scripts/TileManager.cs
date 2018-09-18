020c64ea1eb0fac9803e4a4710047650938b69b4
Andrei <andreialbu50@yahoo.com>
2018-09-18 12:54:06 +0300

    Add Tile Manager & Tile classes

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence
+{
+	public class TileManager : MonoBehaviour 
+	{
+		static TileManager instance = null;
+
+		[SerializeField]
+		GameObject tilePrefab = null;
+
+		[SerializeField]
+		int mapSize;
+
+		List <Tile> tiles = null;
+
+		void Awake()
+		{
+			if(instance == null)
+				instance = this;
+		}
+
+		void Start () 
+		{
+			GenerateTiles();
+		}
+
+		void GenerateTiles()
+		{
+			if(tilePrefab == null)
+				return;
+
+			var tileCount = mapSize * mapSize;
+			tiles = new List <Tile> (tileCount);
+
+			float positionOffset = (float)(mapSize - 1) * 0.5f;
+			for(int i = 0; i < mapSize; ++i)
+			{
+				for(int j = 0; j < mapSize; ++j)
+				{
+					var position = new Vector3((float)i - positionOffset, (float)j - positionOffset, 0.0f);
+					var tileObject = Instantiate(tilePrefab, position, Quaternion.identity);
+					if(tileObject == null)
+						continue;
+
+					var tile = tileObject.GetComponent<Tile>();
+					if(tile == null)
+						continue;
+
+					tiles.Add(tile);
+					tileObject.transform.SetParent(gameObject.transform);
+				}	
+			}
+		}
+	}
+}
