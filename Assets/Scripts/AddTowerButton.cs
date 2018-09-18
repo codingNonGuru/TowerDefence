14b6cdc204d3be3a7aae580622a4a32bb6f652c4
Andrei <andreialbu50@yahoo.com>
2018-09-18 13:37:02 +0300

    Add Buy Tower Button to UI

---
+++ Assets/Scripts/AddTowerButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence
+{
+	public class AddTowerButton : MonoBehaviour 
+	{
+		void Start()
+		{
+			GameManager.OnTowerAddStarted += HandleTowerAddStarted;
+		}
+
+		public void AddTower()
+		{
+			GameManager.StartAddingTower();
+		}
+
+		void HandleTowerAddStarted()
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence
+{
+	public class GameManager : MonoBehaviour 
+	{
+		static GameManager instance = null;
+
+		public static event Action OnTowerAddStarted;
+
+		bool isAddingTower = false;
+
+		int gold = 0;
+
+		public static bool IsAddingTower
+		{
+			get {return instance.isAddingTower;}
+		}
+
+		void Start()
+		{
+			if(instance == null)
+				instance = this;
+		}
+
+		public static void StartAddingTower()
+		{
+			if(instance.isAddingTower)
+				return;
+
+			if(OnTowerAddStarted != null)
+			{
+				OnTowerAddStarted.Invoke();
+			}
+		}
+	}
+}
